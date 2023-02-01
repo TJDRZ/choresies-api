5f6738ee3addc2e756deefa9fea0c98e7f04c4f5
TJ <tjdrzewiecki@gmail.com>
2023-02-01 18:32:33 -0500

    Package installation and barebones server setup

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+const app = express();
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+//app.use(some route)
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server running on port ${port}...`));
