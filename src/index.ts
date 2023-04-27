346ab8837fd60a9fd09b8044cb3ac2b176095859
TJ <tjdrzewiecki@gmail.com>
2023-04-27 17:24:24 -0400

    Added chore routes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import cors from "cors";
 import morgan from "morgan";
 import { connectDB } from "./dbConfig/db.js";
 connectDB();
-import router from "./routes/person.js";
+import personRouter from "./routes/person.js";
+import choreRouter from "./routes/chore.js"
 
 const app = express();
 
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
-app.use("/api/person", router);
+app.use("/api/person", personRouter);
+app.use("/api/chore", choreRouter);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
